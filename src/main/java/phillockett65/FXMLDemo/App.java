d0792cf5d40e5bf3730e85d0fcd425e8b418c8fa
Phil Lockett <phillockett65@gmail.com>
2022-08-30 16:29:27 -0400

    Initial commit, extending BaseFXML.

---
+++ src/main/java/phillockett65/FXMLDemo/App.java
@@ -0,0 +1,71 @@
+/*  FXMLDemo - a JavaFX application 'framework' that uses Maven, FXML and CSS.
+ *
+ *  Copyright 2022 Philip Lockett.
+ *
+ *  This file is part of FXMLDemo.
+ *
+ *  FXMLDemo is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  FXMLDemo is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with FXMLDemo.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/*
+ * Boilerplate code responsible for launching the JavaFX application. 
+ */
+package phillockett65.FXMLDemo;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+
+    PrimaryController controller;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("primary.fxml"));
+
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+        controller = fxmlLoader.getController();
+
+        scene.getStylesheets().add(App.class.getResource("application.css").toExternalForm());
+
+        stage.setTitle("FXMLDemo");
+        stage.setOnCloseRequest(e -> Platform.exit());
+        stage.resizableProperty().setValue(false);
+        stage.setScene(scene);
+
+        stage.show();
+
+        controller.init();
+    }
+
+    @Override
+    public void stop() throws Exception {
+        controller.saveState();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
